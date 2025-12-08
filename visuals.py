fdc1b80823492bc1cc3be4f5c0cc11085d1f20e0
Olivia Lombardo <livlomb@umich.edu>
2025-12-08 17:03:08 -0500

    Updated files.

--- visuals.py
+++ visuals.py
@@ -16,23 +16,20 @@ def load_csv_data(filename):
 # ------------------------------------------------------------
 # Generate Plots
 # ------------------------------------------------------------
-
 def top_15_artist(filename):
     top_artists = load_csv_data('artist_frequency.csv')
     
     top_artists_sorted = sorted(top_artists, key=lambda x: x[1], reverse=False)[:15]
     artists, counts = zip(*top_artists_sorted)
+    counts = [int(c) for c in counts]   # ensure numeric
 
-    fig, ax1 = plt.subplots(figsize=(10,5))
-    ax1.barh(artists, counts, color=colors, align='center')
-
-    ax1.set_xlabel("Artist Count")
-    ax1.set_ylabel("Artist Name")
-    ax1.set_title("Number of Appearances for an Artist in the Billboard Hot 100")
-
+    plt.barh(artists, counts, color=colors)
+    plt.xlim(0, max(counts) + 1)
+    plt.xlabel("Artist Count")
+    plt.ylabel("Artist Name")
+    plt.title("Number of Appearances for an Artist in the Billboard Hot 100")
     plt.tight_layout()
     plt.show()
-
 
 def artist_playcount_pie(filename):
     plays = load_csv_data('artist_playcounts.csv')
